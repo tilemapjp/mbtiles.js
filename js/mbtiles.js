097487e7be55e48176093ccfc246e2252199b576
OHTSUKA Ko-hei <kochizufan@gmail.com>
2013-03-23 20:21:09 +0900

    Change Object URL to data URL

--- js/mbtiles.js
+++ js/mbtiles.js
@@ -124,9 +124,10 @@ var MBTiles = (function(){
         // カラム取り出し
         var blobsize = Module.ccall( 'sqlite3_column_bytes', 'number', ['number', 'number'], [this.tileStmt, 0] );
         var blobptr  = Module.ccall( 'sqlite3_column_blob',  'number', ['number', 'number'], [this.tileStmt, 0] );
-        var blobview = new Uint8Array(HEAPU8.subarray(blobptr, blobptr + blobsize));
-        var blob     = new Blob( [blobview], {type:"image/png"} );
-        returl = URL.createObjectURL(blob);
+        //var blobview = new Uint8Array(HEAPU8.subarray(blobptr, blobptr + blobsize));
+        //var blob     = new Blob( [blobview], {type:"image/png"} );
+        returl = 'data:image/png;base64,' + arrayBufferDataUri(HEAPU8.subarray(blobptr, blobptr + blobsize));
+        //returl = URL.createObjectURL(blob);
         break;
       case 101 :
         break;
@@ -143,6 +144,46 @@ var MBTiles = (function(){
     _free(this.apiTemp);
     this.loaded = false;
   };
+  // Code from http://jsperf.com/encoding-xhr-image-data/6
+  function arrayBufferDataUri(raw) {
+    var base64 = '';
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    var bytes = new Uint8Array(raw);
+    var byteLength = bytes.byteLength;
+    var byteRemainder = byteLength % 3;
+    var mainLength = byteLength - byteRemainder;
+    var a, b, c, d;
+    var chunk;
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3) {
+      // Combine the three bytes into a single integer
+      chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+      // Use bitmasks to extract 6-bit segments from the triplet
+      a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
+      b = (chunk & 258048) >> 12; // 258048 = (2^6 - 1) << 12
+      c = (chunk & 4032) >> 6; // 4032 = (2^6 - 1) << 6
+      d = chunk & 63; // 63 = 2^6 - 1
+      // Convert the raw binary segments to the appropriate ASCII encoding
+      base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
+    }
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+      chunk = bytes[mainLength];
+      a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2;
+      // Set the 4 least significant bits to zero
+      b = (chunk & 3) << 4 // 3 = 2^2 - 1;
+      base64 += encodings[a] + encodings[b] + '==';
+    }
+    else if (byteRemainder == 2) {
+      chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+      a = (chunk & 16128) >> 8 // 16128 = (2^6 - 1) << 8;
+      b = (chunk & 1008) >> 4 // 1008 = (2^6 - 1) << 4;
+      // Set the 2 least significant bits to zero
+      c = (chunk & 15) << 2 // 15 = 2^4 - 1;
+      base64 += encodings[a] + encodings[b] + encodings[c] + '=';
+    }
+    return base64;
+  }
   var Module = {'noInitialRun' : true };
 // Note: For maximum-speed code, see "Optimizing Code" on the Emscripten wiki, https://github.com/kripken/emscripten/wiki/Optimizing-Code
 // Note: Some Emscripten settings may limit the speed of the generated code.
